420a97a3c10436d0edcfb3410767d11b9863b52c
devv-robert <mr.suida@outlook.com>
2024-08-19 13:04:21 -0300

    add: firts codes

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 
 <script setup>
-    import login from './pages/login.vue'
+    import {ref, defineAsyncComponent} from "vue"
+	const current_screen = ref(defineAsyncComponent(() => import('./pages/login.vue')));
 </script>
 
 <template>
-    <component is:login></component>
+    <component :is="current_screen"></component>
 </template>
 
 <style scoped>
---
+++ src/pages/login.vue
@@ -0,0 +1,27 @@
+
+<script></script>
+
+<template>
+    <div>
+        <img class="logo-ic" alt=""/>
+        <h1 class="page-title">Login</h1>
+        <form>
+            <label class="input-form">
+                <span>Email</span>
+                <input type="email" placeholder="type your email"/>
+            </label>
+            <label class="input-form">
+                <span>Password</span>
+                <input type="password" placeholder="type a password"/>
+            </label>
+            <a hre="" class="recovery-link">Recovery password</a>
+            <button class="bnt-submit">Login</button>
+            <p class="new-account-link">Dont have an accounter?, <a href='' class="new-ac-link">sing up here</a></p>
+        </form>
+    </div>
+    <footer>
+        CraftNerd &copy; - 2023/2024
+    </footer>
+</template>
+
+<style scoped></style>
