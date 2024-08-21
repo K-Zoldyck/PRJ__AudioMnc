3ec855b1901a152f6ceb6f1851da4537be1fd6de
devv-robert <mr.suida@outlook.com>
2024-08-21 10:25:58 -0300

    add: login firts styles

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -3,7 +3,7 @@
 
 <template>
     <div>
-        <img class="logo-ic" alt=""/>
+        <img class="logo-ic" alt="" src="./icons/ic_aber.png"/>
         <h1 class="page-title">Login</h1>
         <form>
             <label class="input-form">
@@ -24,4 +24,40 @@
     </footer>
 </template>
 
-<style scoped></style>
+<style scoped>
+    .logo-ic{
+        display: block;
+        width: 20%;
+        margin: 10% auto 4% auto;
+    }
+    .page-title {
+        width: 100%;
+        text-align: center;
+        font: 600 1.9rem/1 sans-serif;
+    }
+    form {
+        width: 80%;
+        margin: 10% auto 14% auto;
+    }
+        .input-form {
+            display: block;
+            width: 100%;
+            margin: 6% 0;
+        }
+            .input-form span {
+                display: block;
+                font: 600 1.3rem/1 sans-serif;
+                padding-bottom: 1%;
+            }
+            .input-form input{
+                display: block;
+                border: 1px solid #312e2e;
+                padding: 4% 2%;
+                width: 100%;
+                outline: none;
+                font: 200 .9rem/1 sans-serif;
+            }
+            .input-form input[type='password'] { background: url('./icons/ic_pass.svg') 95% 50% / 5% no-repeat;}
+            .input-form input[type='email']{ background: url('./icons/ic_emil.svg') 95% 50% / 5% no-repeat;}
+
+</style>
